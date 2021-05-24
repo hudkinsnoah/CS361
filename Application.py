7f2dc9fd480b961673a4392599a12255302ad9d2
hudkinsnoah <hudkinsn@oregonstate.edu>
2021-05-24 12:04:01 -0700

    Update Application.py
    Next push of my data to repository. Added bug fixes and implemented the ability to update the data displayed in the main GUI

--- Application.py
+++ Application.py
@@ -2,6 +2,9 @@ import tkinter as tk
 from tkinter import *
 import tkinter.font as tkFont
 import tkinter.messagebox
+import mysql.connector
+import requests
+import json
 
 
 def exitProtocol(self):
@@ -30,7 +33,12 @@ class setUp(tk.Tk):
         questionsAnswered.set(1)
         global questionsCorrect
         questionsCorrect = tk.IntVar()
-        questionsCorrect.set(10)
+        questionsCorrect.set(0)
+        global percentCorrect
+        percentCorrect = tk.StringVar()
+        percentCorrect.set("0.0")
+        global requestmade
+        requestmade=False
 
         container.pack(side="top", fill="both", expand=True)
 
@@ -90,9 +98,9 @@ class settingsPage(tk.Frame):
             global currentTimeline
             currentTimeline.set(clicked1.get())
             global numquestions
-            numquestions = clicked2.get()
+            numquestions.set(clicked2.get())
 
-            if numquestions >= "40":
+            if numquestions.get() >= 40:
                 numareyousure = tkinter.messagebox.askquestion("Are you sure?",
                                                                "You have selected the option to have over 40 questions."
                                                                " This may take you a long time to complete. "
@@ -101,7 +109,7 @@ class settingsPage(tk.Frame):
                 if numareyousure == "yes":
                     pass
                 else:
-                    numquestions=15
+                    numquestions.set(15)
                     clicked2.set(numQuestionsOptions[2])
 
         homeButton = Button(self, text="Home", width=10, height=1, padx=10, pady=10,
@@ -156,9 +164,18 @@ class settingsPage(tk.Frame):
         selectButton.pack(side=BOTTOM)
 
 
+class Question:
+  def __init__(self, text, date):
+    self.text = tk.StringVar()
+    self.text.set(text)
+    self.date = tk.StringVar()
+    self.date.set(date)
+
+
 class questionsPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+
 
         def homeProtocol():
             exitOption = tkinter.messagebox.askquestion("Exit To Home?", "Are you sure you want to quit the test? "
@@ -171,15 +188,98 @@ class questionsPage(tk.Frame):
                 questionsAnswered.set(1)
                 controller.show_frame(startPage)
 
+        def conversion(change):
+            if change == "1st":
+                return 1
+            elif change == "2nd":
+                return 2
+            elif change == "3rd":
+                return 3
+            else:
+                return 4
+
+        def checkCorrect():
+            ans1 = conversion(clicked1.get())
+            ans2 = conversion(clicked2.get())
+            ans3 = conversion(clicked3.get())
+            ans4 = conversion(clicked4.get())
+
+            answersConverted = [ans1, ans2, ans3, ans4]
+
+            questions1 = question1txt.date.get()
+            questions2 = question2txt.date.get()
+            questions3 = question3txt.date.get()
+            questions4 = question4txt.date.get()
+
+            questions = [questions1, questions2, questions3, questions4]
+            questionsSorted = sorted(questions)
+
+            for i in range(0, 4):
+                if answersConverted[i] == questionsSorted.index(questions[i]) + 1:
+                    pass
+                else:
+                    return False
+            return True
+
+        def updatequestions():
+            global requestmade
+            request = {}
+            request = requests.get("http://flip3.engr.oregonstate.edu:6952/query")
+            requestmade = True
+            response = request.json()
+            question1txt.text.set(response[0]['name'])
+            question1txt.date.set(response[0]['date'])
+            question2txt.text.set(response[1]['name'])
+            question2txt.date.set(response[1]['date'])
+            question3txt.text.set(response[2]['name'])
+            question3txt.date.set(response[2]['date'])
+            question4txt.text.set(response[3]['name'])
+            question4txt.date.set(response[3]['date'])
+
+            clicked1.set(Answers[0])
+            clicked2.set(Answers[0])
+            clicked3.set(Answers[0])
+            clicked4.set(Answers[0])
+
+        def updateServer():
+            db = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                passwd="Celticsbasketball2001.",
+                database="mainbase"
+            )
+
+            cur = db.cursor()
+
+            global numquestions
+            global questionsCorrect
+            global percentCorrect
+
+
+            cur.execute("INSERT INTO finaltab (numbercorrect, numberofquestions, percentage) VALUES (%s, %s, %s)",
+                        (questionsCorrect.get(), numquestions.get(), percentCorrect.get()))
+            db.commit()
+            controller.show_frame(resultsPage)
+
         def endTest():
             global numquestions
             global questionsAnswered
+            global percentCorrect
+            global questionsCorrect
             questionsAnswered.set(questionsAnswered.get() + 1)
+            correct = checkCorrect()
+            if correct:
+                questionsCorrect.set(questionsCorrect.get() + 1)
+
             if questionsAnswered.get() > numquestions.get():
                 questionsAnswered.set(1)
-                controller.show_frame(resultsPage)
+                percentCorrect.set(str((questionsCorrect.get() / numquestions.get()) * 100) + "%")
+                updateServer()
+
             else:
+                updatequestions()
                 pass
+
 
         homeButton = Button(self, text="Home", width=10, height=1, padx=10, pady=10, command=homeProtocol)
         homeButton.pack(side=LEFT, anchor=NW)
@@ -203,29 +303,32 @@ class questionsPage(tk.Frame):
         questionframe.place(relx=.1, rely=1, anchor=S)
         nextframe.place(relx=.95, rely=1, anchor=S)
 
-        question1 = Label(QuestionFrame, text="This is option 1. The date of this question happened in 1970",
-                          font=fontStyle)
+        question1txt = Question("", 0)
+        question1 = Label(QuestionFrame, textvariable=question1txt.text, font=fontStyle)
         question1.pack()
         spacer1 = Label(QuestionFrame, text="")
         spacer1.pack()
-        question2 = Label(QuestionFrame, text="This is option 2. The data of this question happened in 1980",
-                          font=fontStyle)
+
+        question2txt = Question("", 0)
+        question2 = Label(QuestionFrame, textvariable=question2txt.text, font=fontStyle)
         question2.pack()
         spacer2 = Label(QuestionFrame, text="")
         spacer2.pack()
-        question3 = Label(QuestionFrame, text="This is option 3. The date of this question happened in 1960",
-                          font=fontStyle)
+
+        question3txt = Question("", 0)
+        question3 = Label(QuestionFrame, textvariable=question3txt.text, font=fontStyle)
         question3.pack()
         spacer3 = Label(QuestionFrame, text="")
         spacer3.pack()
-        question4 = Label(QuestionFrame, text="This is option 4. The date of this question happened in 1955",
-                          font=fontStyle)
+
+        question4txt = Question("", 0)
+        question4 = Label(QuestionFrame, textvariable=question4txt.text, font=fontStyle)
         question4.pack()
 
-        Answers = ["Option 1",
-                   "Option 2",
-                   "Option 3",
-                   "Option 4"]
+        Answers = ["1st",
+                   "2nd",
+                   "3rd",
+                   "4th"]
 
         clicked1 = StringVar()
         clicked1.set(Answers[0])
@@ -254,14 +357,29 @@ class questionsPage(tk.Frame):
         global questionsAnswered
         fontStyle = tkFont.Font(family="lucida Grande", size=20)
         CurQuestion = Label(questionframe, textvariable=questionsAnswered, font=fontStyle)
-        spacerlabel = Label(questionframe, text="/", font = fontStyle)
-        totalQuestions = Label(questionframe, text=numquestions.get(), font=fontStyle)
+        spacerlabel = Label(questionframe, text="/", font=fontStyle)
+        totalQuestions = Label(questionframe, textvariable=numquestions, font=fontStyle)
         CurQuestion.pack(side=LEFT)
         spacerlabel.pack(side=LEFT)
         totalQuestions.pack(side=LEFT)
 
         NextButton = Button(nextframe, text="Next", font=fontStyle, bg = "Orange", command=endTest)
         NextButton.pack(side=RIGHT)
+
+        global requestmade
+        request = {}
+        if requestmade == False:
+            request = requests.get("http://flip3.engr.oregonstate.edu:6952/query")
+            requestmade = True
+        response = request.json()
+        question1txt.text.set(response[0]['name'])
+        question1txt.date.set(response[0]['date'])
+        question2txt.text.set(response[1]['name'])
+        question2txt.date.set(response[1]['date'])
+        question3txt.text.set(response[2]['name'])
+        question3txt.date.set(response[2]['date'])
+        question4txt.text.set(response[3]['name'])
+        question4txt.date.set(response[3]['date'])
 
 
 class pausePage(tk.Frame):
@@ -303,6 +421,15 @@ class pausePage(tk.Frame):
 
 class resultsPage(tk.Frame):
     def __init__(self, parent, controller):
+
+        def percentfinished():
+            global numquestions
+            global questionsCorrect
+
+            total = (questionsCorrect.get() / numquestions.get()) * 100
+            return total
+
+
         tk.Frame.__init__(self,parent)
         exitButton = Button(self, text="Exit", width=10, height=1, padx=10, pady=10, command=lambda: exitProtocol(self))
         exitButton.pack(side=LEFT, anchor=NW)
@@ -327,17 +454,20 @@ class resultsPage(tk.Frame):
 
         global questionsCorrect
         global numquestions
+        global percentCorrect
         fontStyle = tkFont.Font(size=16)
         numCorrect = Label(keyframe, text="Total Correct:", font=fontStyle)
         numCorrect.pack()
-        totalLabel = Label(resultframe, text=str(questionsCorrect.get()) + "/" + str(numquestions.get()), font=fontStyle)
-        totalLabel.pack()
-        percentCorrect = Label(keyframe, text="Percent Correct:", font=fontStyle)
-        percentCorrect.pack()
-        number = (questionsCorrect.get() / numquestions.get()) * 100
-        PercentLabel = Label(resultframe, text=str(number) + "%", font=fontStyle)
-        PercentLabel.pack()
-        totalLabel.pack()
+        CurQuestion = Label(resultframe, textvariable=questionsCorrect, font=fontStyle)
+        spacerlabel = Label(resultframe, text="/", font=fontStyle)
+        totalQuestions = Label(resultframe, textvariable=numquestions, font=fontStyle)
+        percentCorrectlbl = Label(keyframe, text="Percent Correct:", font=fontStyle)
+        percentCorrectlbl.pack()
+        PercentLabel = Label(resultframe, textvariable=percentCorrect, font=fontStyle)
+        PercentLabel.pack(side=BOTTOM)
+        CurQuestion.pack(side=LEFT)
+        spacerlabel.pack(side=LEFT)
+        totalQuestions.pack(side=LEFT)
 
 
 app = setUp()
